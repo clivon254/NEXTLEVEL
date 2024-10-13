789025a2d71ae5a85fface1a2a9b9583a7461b43
clivon254 <clivon254@icloud.com>
2024-10-13 07:57:56 +0300

    sever utlis

--- server/index.js
+++ server/index.js
@@ -19,4 +19,39 @@ app.use(express.json())
 
 
 // DB CONECTION
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB CONNECTED"))
+.catch((err) => console.log(err))
 
+
+
+//API
+app.get('/',(req,res) => {
+
+    res.send("HELLO NEXT NEXTLEVEL")
+
+})
+
+
+
+//APP LISTENING
+app.listen(PORT,() => {
+
+    console.log(`server running on port  ${PORT}`)
+
+})
+
+
+app.use((err,req,res,next) => {
+
+    const statusCode = err.statusCode || 500
+
+    const message = err.message || "Internal Server Error"
+
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+
+})
---
+++ server/utils/verify.js
@@ -0,0 +1,26 @@
+
+import { errorHandler } from "./error.js"
+import jwt from jsonwebtoken
+
+
+export const verifyToken = async (req,res,next) => {
+
+    const token = req.headers
+
+    if(!token)
+    {
+        return next(errorHandler(403, "forbiden"))
+    }
+
+    jwt.verify(token, process.env.JWT_SECRETE,(err, user) => {
+
+        if(err)
+        {
+            return next(errorHandler(401,"unauthorized"))
+        }
+
+        req.user = user
+
+        next()
+    })
+}
