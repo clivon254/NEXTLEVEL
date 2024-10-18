345a3140b2a098006634191d9dbb25b79fa7142f
clivon254 <clivon254@icloud.com>
2024-10-18 06:29:38 +0300

    product list

--- client/src/context/store.jsx
+++ client/src/context/store.jsx
@@ -1,5 +1,6 @@
 
 
+import axios from 'axios'
 import React, { createContext, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
@@ -17,12 +18,64 @@ export default function StoreContextProvider(props){
 
   const [open, setOpen] = useState(false)
 
+  const [products, setProducts] = useState([])
+
+  const [productsError, setproductsError] = useState(false)
+
+  const [productsLoading, setProductsLoading] = useState(false)
+
+  const [showModal, setShowModel] = useState(false)
+
+  // fetchProducts
+  const fetchProducts  = async () => {
+
+    try
+    {
+      setProductsLoading(true)
+
+      setproductsError(false)
+
+      const res = await axios.get(url + "/api/product/get-products")
+
+      if(res.data.success)
+      {
+        setProductsLoading(false)
+
+        setproductsError(false)
+
+        setProducts(res.data.products)
+      }
+      else
+      {
+        setProductsLoading(false)
+
+        setProductsLoading(false)
+
+        console.log(error.message)
+      }
+
+    }
+    catch(error)
+    {
+      setProductsLoading(false)
+
+      setProductsLoading(false)
+
+      console.log(error.message)
+    }
+
+  }
+
+ 
+
   useEffect(() => {
 
     if(localStorage.getItem("token"))
     {
       setToken(localStorage.getItem("token"))
     }
+
+    fetchProducts()
 
   },[])
 
@@ -31,7 +84,14 @@ export default function StoreContextProvider(props){
     open,
     setOpen,
     token,
-    setToken
+    setToken,
+    products,
+    setProducts,
+    productsError,
+    setproductsError,
+    productsLoading,
+    setProductsLoading,
+    fetchProducts,
   }
  
 
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,9 +1,275 @@
 
 
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { StoreContext } from '../context/store'
+import axios from 'axios'
+import { Table } from 'flowbite-react'
+import { Link } from 'react-router-dom'
+import { MdEdit, MdViewArray } from 'react-icons/md'
+import { FaTrash } from "react-icons/fa"
 
 export default function Products() {
+  
+  const {currentUser} = useSelector(state => state.user)
+
+  const {url,token,products,setProducts,productLoading,productsError,setShowModal} = useContext(StoreContext)
+
+  const [productIdToDelete, setProductIdToDelete] = useState(null)
+
+  const [loader, setLoader] = useState([{},{},{},{},{},{}])
+
+  // handleDeletePost
+  const handleDeletePost = async () => {
+
+    try
+    {
+      const res = await axios.delete(url + `/api/product/delete-product/${productIdToDelete}`,{headers:{token}})
+
+      if(res.data.success)
+      {
+        setProducts((prev) => 
+              prev.filter((post) => post._id !== productIdToDelete)
+        )
+
+        setShowModal(false)
+
+        toast.success('Post deleted successfuly')
+      }
+
+     }
+    catch(error)
+    {
+      console.log(error.message)
+    }
+
+  }
+
   return (
-    <div>Products</div>
+    
+    <section className="section">
+
+      <h2 className="title text-center">Products</h2>
+
+      <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+
+        {!productLoading && !productsError && (
+
+          <div className="">
+            
+            {products?.length > 0 ? 
+              (
+                <Table>
+                   
+                   <Table.Body className="divide-y font-bold uppercase">
+
+                    <Table.Row >
+
+                      <Table.Cell></Table.Cell>
+
+                      <Table.Cell>Date updated</Table.Cell>
+
+                      <Table.Cell>Image</Table.Cell>
+
+                      <Table.Cell>title</Table.Cell>
+
+                      <Table.Cell>tag1</Table.Cell>
+
+                      <Table.Cell>tag2</Table.Cell>
+
+                      <Table.Cell>Action</Table.Cell>
+
+                    </Table.Row>
+
+                   </Table.Body>
+
+                   {products.map((product,index) => (
+
+                     <Table.Body>
+
+                      <Table.Row>
+
+                        <Table.Cell>{index +1}.</Table.Cell>
+
+                        <Table.Cell>
+                          {new Date(product.createdAt).toLocaleDateString()}
+                        </Table.Cell>
+
+                        <Table.Cell>
+
+                          <img 
+                            src={product.images[0]}
+                            alt="" 
+                            className="w-20 h-10"
+                          />
+
+                        </Table.Cell>
+
+                        <Table.Cell>
+                          {product.name}
+                        </Table.Cell>
+
+                        <Table.Cell>
+                          {product.tag1}
+                        </Table.Cell>
+
+                        <Table.Cell>
+                          {product.tag2}
+                        </Table.Cell>
+
+                        <Table.Cell>
+
+                          <div className="flex item-center gap-x-2">
+
+                            <span className="">
+
+                              <Link to={`/product/${product._id}`}>
+
+                                <MdViewArray size={20}/>
+
+                              </Link>
+
+                            </span>
+
+                            <span className="">
+
+                              <Link to={`/edit-product/${product._id}`}>
+
+                                  <MdEdit size={20}/>
+
+                              </Link>
+
+                            </span>
+
+                            <span className="">
+
+                              <FaTrash
+                                size={20}
+                                onClick={() => {setShowModal; setProductIdToDelete(product._id)}}
+                              />
+
+                            </span>
+
+                          </div>
+
+                        </Table.Cell>
+
+                      </Table.Row>
+                      
+                     </Table.Body>
+
+                   ))}
+
+                </Table>
+              ) 
+              : 
+              (
+                <p className="text-center font-semibold mt-20">No products yet</p>
+              )
+            }
+
+          </div>
+
+        )}
+
+        {productLoading && !productsError && (
+
+          <Table>
+                   
+            <Table.Body className="divide-y font-bold uppercase">
+
+              <Table.Row >
+
+                <Table.Cell></Table.Cell>
+
+                <Table.Cell>Date updated</Table.Cell>
+
+                <Table.Cell>Image</Table.Cell>
+
+                <Table.Cell>title</Table.Cell>
+
+                <Table.Cell>tag1</Table.Cell>
+
+                <Table.Cell>tag2</Table.Cell>
+
+                <Table.Cell>Action</Table.Cell>
+
+              </Table.Row>
+
+            </Table.Body>
+
+              {loader.map((product,index) => (
+
+                <Table.Body>
+
+                  <Table.Row>
+
+                    <Table.Cell>{index +1}.</Table.Cell>
+
+                    <Table.Cell >
+
+                      <div className="LoadingBg h-5 w-10"/>
+
+                    </Table.Cell>
+
+                    <Table.Cell>
+
+                       <div className="LoadingBg h-5 w-10"/>  
+
+                    </Table.Cell>
+
+                    <Table.Cell>
+
+                        <div className="LoadingBg h-14 w-10"/>
+
+                    </Table.Cell>
+
+                    <Table.Cell>
+
+                         <div className="LoadingBg h-5 w-10"/>
+
+                    </Table.Cell>
+
+                    <Table.Cell>
+
+                      <div className="LoadingBg h-5 w-10"/>
+
+                    </Table.Cell>
+
+                    <Table.Cell>
+
+                       <div className="LoadingBg h-5 w-10"/>
+
+                    </Table.Cell>
+
+                  </Table.Row>
+                
+                </Table.Body>
+
+              ))}
+
+          </Table>
+
+        )}
+
+        {!productLoading && productsError && (
+
+          <div className=" mt-20">
+
+              <p className="">Something went wrong</p>
+
+              <p className=""></p>
+
+              <span className="btn" onClick={fetchProduct()}>retry</span>
+
+          </div>
+
+        )}
+
+      </div>
+
+    </section>
+
   )
+
 }
