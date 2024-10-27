c27ada8fbd405814babec7a44d03ffad6d93778b
clivon254 <clivon254@icloud.com>
2024-10-27 20:13:16 +0300

    svg

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,13 +2,22 @@
 
 
 import React from 'react'
+import Logo from "../assets/Logo.png"
+
+
 
 export default function Logo() {
 
   return (
 
     <div className=' text-xl sm:text-2xl lg:text-3xl  2xl:text-4xl font-serif'>
-        NEXT LEVEL 
+        
+        <img 
+          src={Logo}
+          alt="" 
+          className="h-20 w-20" 
+        />
+
     </div>
 
   )
